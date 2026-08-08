f945222281cecfb493adde0db3f42e7fa3c1c019
helmymalak92@gmail.com <helmymalak92@gmail.com>
2026-08-08 14:22:48 +0300

    init

--- MyRestaurantApp/MyRestaurantApp.Application/Users/Dtos/LoginRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace MyRestaurantApp.Application.Users.Dtos
-{
-    public class LoginRequest
-    {
-        public string Email { get; set; } = null!;
-        public string Password { get; set; } = null!;
-    }
-}
